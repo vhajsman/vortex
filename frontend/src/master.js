fb20e8068852ff6630acb824023f3d8fc5669a96
vhajsman <vaclav.hajsman@hotmail.com>
2025-03-09 23:55:13 +0100

    modified:   frontend/general.css 	modified:   frontend/index.html 	modified:   frontend/src/master.js 	frontend/src/desktop.js 	frontend/src/wnx.js

--- frontend/src/master.js
+++ frontend/src/master.js
@@ -4,27 +4,38 @@ import { vrotex_ping } from "./ping.js"
 
 console.log(`Vortex server ping: ${await vrotex_ping()} ms`);
 
-document.getElementById("vortex-loging-login-incorrect").style.display = "none";
 vortex_desktop.desktopElement.style.display = "none";
 
-document.getElementById("vortex-login-loginbtn").addEventListener("click", async () => {
-    const form = document.getElementById("vortex-form-login-login");
-    const username = form["vortex-login-login-username"].value;
-    const password = form["vortex-login-login-password"].value;
+const urlPar = new URLSearchParams(window.location.search);
+if(urlPar.has("f-dbg-nologin")) {
+    console.log("skipping login because of debug flag");
+    document.getElementById("vortex-loging-login-incorrect").style.display = "none";
+    document.getElementById("vortex-activity-login").style.display = "none";
 
-    try {
-        const r = await vortex_logon.login(username, password);
-        if (r) {
-            document.getElementById("vortex-loging-login-incorrect").style.display = "none";
-            document.getElementById("vortex-activity-login").style.display = "none";
-
-            vortex_desktop.main();
-            vortex_taskbar.main();
-        } else {
-            document.getElementById("vortex-loging-login-incorrect").style.display = "block";
+    vortex_desktop.main();
+    vortex_taskbar.main();
+} else {
+    document.getElementById("vortex-loging-login-incorrect").style.display = "none";
+    
+    document.getElementById("vortex-login-loginbtn").addEventListener("click", async () => {
+        const form = document.getElementById("vortex-form-login-login");
+        const username = form["vortex-login-login-username"].value;
+        const password = form["vortex-login-login-password"].value;
+    
+        try {
+            const r = await vortex_logon.login(username, password);
+            if (r) {
+                document.getElementById("vortex-loging-login-incorrect").style.display = "none";
+                document.getElementById("vortex-activity-login").style.display = "none";
+    
+                vortex_desktop.main();
+                vortex_taskbar.main();
+            } else {
+                document.getElementById("vortex-loging-login-incorrect").style.display = "block";
+            }
+        } catch (e) {
+            console.error("Login error:", e);
+            alert("Login error: " + e.message);
         }
-    } catch (e) {
-        console.error("Login error:", e);
-        alert("Login error: " + e.message);
-    }
-});
+    });
+}
