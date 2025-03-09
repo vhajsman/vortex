fb1876551ce3078b2b7240dec5f3e2e7346ab32d
vhajsman <vaclav.hajsman@hotmail.com>
2025-03-09 21:15:26 +0100

    complete login form

--- frontend/src/master.js
+++ frontend/src/master.js
@@ -1,3 +1,22 @@
+import { vortex_logon } from "./logon.js";
 import { vrotex_ping } from "./ping.js"
 
 console.log(`Vortex server ping: ${await vrotex_ping()} ms`);
+
+document.getElementById("vortex-login-loginbtn").addEventListener("click", async () => {
+    const form = document.getElementById("vortex-form-login-login");
+    const username = form["vortex-login-login-username"].value;
+    const password = form["vortex-login-login-password"].value;
+
+    try {
+        const r = await vortex_logon.login(username, password);
+        if (r) {
+            alert("Logged in");
+        } else {
+            alert("Login failed: Invalid credentials");
+        }
+    } catch (e) {
+        console.error("Login error:", e);
+        alert("Login error: " + e.message);
+    }
+});
